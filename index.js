7267e67b7c33396e8acee0343d871141a15e1215
Judith Ramirez <rqmirez@gmail.com>
2022-03-06 13:29:25 -0500

    commit tree 0c2f30a6700a8a745d3956b41a16af852983cc1b

---
+++ index.js
@@ -0,0 +1,57 @@
+function add() {
+}
+function subtract(){
+}
+function multiply(){
+}
+function divide(){
+}
+function add() {
+    return "something";
+}
+function add(a, b) {
+    return "something";
+}
+function add(a, b) {
+    return a +b;
+}
+function subtract() {
+    return "something";
+}
+function subtract(a, b) {
+    return "something";
+}
+function subtract(a,b) {
+    return a - b;
+}
+function multiply() {
+    return "something"
+}
+function multiply(a, b) {
+    return "something"
+}
+function multiply(a, b) {
+    return a * b
+}
+function divide() {
+    return "something"
+}
+function divide(a, b) {
+    return "something"
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(a) {
+    return (a + 1)
+}
+function decrement(a) {
+    return (a - 1)
+}
+function makeInt(a) {
+    parseInt(a, 10)
+    return parseInt(a, 10)
+}
+function preserveDecimal(a) {
+    return parseFloat(a)
+}
